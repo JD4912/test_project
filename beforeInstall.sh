0e3ce3d28925380518348b3f7433de7a18472e29
JD4912 <58825633+JD4912@users.noreply.github.com>
2022-05-04 18:33:54 +0530

    Update beforeInstall.sh

--- beforeInstall.sh
+++ beforeInstall.sh
@@ -1,24 +1,11 @@
 #!/bin/bash
-sudo su - 
-curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo add-apt-repository ppa:chris-lea/redis-server
-sudo apt-get update
-sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev software-properties-common libffi-dev dirmngr gnupg apt-transport-https ca-certificates redis-server redis-tools nodejs yarn
-git clone https://github.com/rbenv/rbenv.git ~/.rbenv
-echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
-echo 'eval "$(rbenv init -)"' >> ~/.bashrc
-git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
-echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
-git clone https://github.com/rbenv/rbenv-vars.git ~/.rbenv/plugins/rbenv-vars
-exec $SHELL
-rbenv install 2.7.2
-rbenv global 2.7.2
-gem install bundler
-
-sudo rm -rf /home/ubuntu/rails
-sudo git clone https://github.com/JD4912/test_project.git /home/ubuntu/rails
-sudo cd /home/ubuntu/rails && rm Gemfile.lock
-bundle install
-rails server -b 0.0.0.0
+- echo "remove old files"
+- rm -rf /home/ubuntu/rails
+- echo "cloning New files using github repo"
+- git clone https://github.com/JD4912/test_project.git /home/ubuntu/rails
+- echo "change the working directory and delete Gemfile.lock"
+- sudo cd /home/ubuntu/rails && rm Gemfile.lock\
+- echo "Install the budle package"
+- bundle install
+- echo "Run the Rails server"
+- rails server -b 0.0.0.0
